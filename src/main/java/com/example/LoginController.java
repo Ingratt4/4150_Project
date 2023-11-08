da43e325c5787f4810e15412d6405ae09e4d8063
Cameron Ingratta <cameroningratta@hotmail.com>
2023-11-08 12:36:36 -0500

    Unstable Conncetion

--- src/main/java/com/example/LoginController.java
+++ src/main/java/com/example/LoginController.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -17,9 +20,33 @@ public class LoginController {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        // Implement your login logic here.
-        // You can check if the username and password are valid.
-        // If valid, navigate to the main application screen.
-        // If not valid, show an error message.
+        // Authenticate the user
+        if (isUserAuthenticated(username, password)) {
+            // Navigate to the main application screen (you can replace this with your code)
+            System.out.println("Login successful!");
+        } else {
+            // Show an error message for failed login
+            System.out.println("Login failed. Please check your credentials.");
+        }
     }
+
+    private boolean isUserAuthenticated(String username, String password) {
+        // Connect to the MongoDB database
+        MongoDatabase database = MongoConnection.getDatabase();
+        System.out.println("Database Name: " + database.getName());
+    
+        // Get the collection containing user credentials
+        MongoCollection<Document> collection = database.getCollection("Login-Info");
+        System.out.println("Collection Name: " + collection.getNamespace().getCollectionName()); 
+    
+        // Create a query to find a document matching the provided username and password
+        Document query = new Document("UserName", username).append("Password", password);
+        System.out.println("Query: " + query);
+    
+        // Check if a matching document was found
+        Document userDocument = collection.find(query).first();
+    
+        return userDocument != null;
+    }
+    
 }
