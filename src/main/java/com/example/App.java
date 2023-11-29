e91086c17d6ecf86b8d67d687d0c972bdbd5dfaa
lb975 <bahirat@uwindsor.ca>
2023-11-29 12:01:45 -0500

    Adjusted window size

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -26,7 +26,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Login"), 640, 480);
+        scene = new Scene(loadFXML("Login"), 960, 600);
         stage.setScene(scene);
         stage.show();
     }
